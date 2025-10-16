af868e436dd604b274af74655c201aeb3178b216
Lucas_Adriano <lucassantos30.ls@gmail.com>
2025-10-16 20:01:44 -0300

    Ciração inicial dos componentes do projeto

---
+++ src/app/pages/users-managments/users-managments.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-managments',
+  imports: [],
+  templateUrl: './users-managments.html',
+  styleUrl: './users-managments.scss'
+})
+export class UsersManagments {
+
+}
