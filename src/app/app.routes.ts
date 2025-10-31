28a38a85bbc6c048f5327336082640f74877cb01
MrClaro <adryan.contatoprofissional@gmail.com>
2025-10-31 02:39:55 -0300

    feat(route): adiciona configuração inicial das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Layout } from './components/core/layout/layout';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
