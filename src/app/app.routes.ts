69d022a4a534d25831656f5ead62dcf8f75e3893
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-01 04:06:47 -0300

    feat(route): configura rota de postagem de discussões para testes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Layout } from './components/core/layout/layout';
 import { Home } from './pages/home/home';
+import { PostDiscussion } from './pages/post-discussion/post-discussion';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'home',
         component: Home,
+      },
+      {
+        path: 'discussoes',
+        component: PostDiscussion,
       },
     ],
   },
