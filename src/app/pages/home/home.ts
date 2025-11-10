f65bd0cb9d491a53fa6a46c8c833085e9a155073
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-10 19:52:40 -0300

    refactor(page): refatora página home para testes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { Select } from '../../components/select/select';
-import { SelectOptionI } from '../../interfaces/SelectOptionI';
+import { Select } from '../../components/features/select/select';
+import { PrimaryInput } from '../../components/features/primary-input/primary-input';
+import { ISelectOption } from '../../interfaces/select-option';
 
 @Component({
   selector: 'app-home',
-  imports: [Select],
+  imports: [Select, PrimaryInput],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   estadoSelecionado: string = '';
 
-  estados: SelectOptionI[] = [
+  estados: ISelectOption[] = [
     { value: 'sp', label: 'São Paulo' },
     { value: 'rj', label: 'Rio de Janeiro' },
     { value: 'mg', label: 'Minas Gerais' },
